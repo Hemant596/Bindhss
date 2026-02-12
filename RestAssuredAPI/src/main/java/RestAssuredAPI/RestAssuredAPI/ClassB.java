cb96e40c6bd01e71477a85db86d86d507fe605d7
hemantsh <hemantsh@ICS-LAP-8707>
2026-02-12 13:44:21 +0530

    change in class B only

--- RestAssuredAPI/src/main/java/RestAssuredAPI/RestAssuredAPI/ClassB.java
+++ RestAssuredAPI/src/main/java/RestAssuredAPI/RestAssuredAPI/ClassB.java
@@ -1,5 +1,9 @@
 package RestAssuredAPI.RestAssuredAPI;
 
 public class ClassB {
+	public static void main(String[] args) {
+		
+		System.out.println("Hi this change is only in the classs itself");
+	}
 
 }
